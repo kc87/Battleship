18fbc5d785785e819e170001cf009cc622cdc034
kc87 <me@example.com>
2015-03-15 18:57:46 +0100

    Changed default state transition action

--- src/controller/state/GameState.java
+++ src/controller/state/GameState.java
@@ -1,36 +1,34 @@
 package controller.state;
 
-import org.pmw.tinylog.Logger;
-
 public interface GameState
 {
    default void startNetReceiver()
    {
-      Logger.error("Invalid state transition");
+      throw new IllegalStateException("Invalid state transition");
    }
 
    default void connectPeer()
    {
-      Logger.error("Invalid state transition");
+      throw new IllegalStateException("Invalid state transition");
    }
 
    default void disconnectPeer()
    {
-      Logger.error("Invalid state transition");
+      throw new IllegalStateException("Invalid state transition");
    }
 
    default void newGame()
    {
-      Logger.error("Invalid state transition");
+      throw new IllegalStateException("Invalid state transition");
    }
 
    default void abortGame()
    {
-      Logger.error("Invalid state transition");
+      throw new IllegalStateException("Invalid state transition");
    }
 
    default void stopNetReceiver()
    {
-      Logger.error("Invalid state transition");
+      throw new IllegalStateException("Invalid state transition");
    }
 }
