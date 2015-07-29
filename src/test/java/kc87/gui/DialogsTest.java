00fd1495bdf74af49b288c825c4eb2a60302cc1e
kc87 <kc87@example.com>
2015-07-29 15:09:07 +0200

    Added parameterized test using JUintParam

--- src/test/java/kc87/gui/DialogsTest.java
+++ src/test/java/kc87/gui/DialogsTest.java
@@ -1,37 +1,50 @@
 package kc87.gui;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 
 import java.util.Optional;
 
+@RunWith(JUnitParamsRunner.class)
 public class DialogsTest
 {
-   @Test
-   public void testGetLocalIpFromId() throws Exception
-   {
-      Optional<String> id = Optional.empty();
-      assertThat(Dialogs.getLocalIpFromId(id),equalTo(Optional.empty()));
 
-      id = Optional.of("a12");
-      assertThat(Dialogs.getLocalIpFromId(id),equalTo(Optional.empty()));
-
-      id = Optional.of("-12");
-      assertThat(Dialogs.getLocalIpFromId(id),equalTo(Optional.empty()));
-
-      id = Optional.of("12.89");
-      assertThat(Dialogs.getLocalIpFromId(id),equalTo(Optional.empty()));
-
-      id = Optional.of("0");
-      assertThat(Dialogs.getLocalIpFromId(id),equalTo(Optional.empty()));
-
-      id = Optional.of("255");
-      assertThat(Dialogs.getLocalIpFromId(id),equalTo(Optional.empty()));
-
-      id = Optional.of("201");
-      assertThat(Dialogs.getLocalIpFromId(id).get(),equalTo("127.0.0.201"));
+   @SuppressWarnings("unused")
+   private Object[] getInvalidTestIds() {
+      return new Object[] {
+              Optional.empty(),
+              Optional.of("a12"),
+              Optional.of("-12"),
+              Optional.of("12.89"),
+              Optional.of("0"),
+              Optional.of("255")
+      };
    }
 
+   @SuppressWarnings("unused")
+   private Object[] getValidTestIds() {
+      return new Object[] {
+        new Object[]{ Optional.of("1"), "127.0.0.1" },
+        new Object[]{ Optional.of("35"), "127.0.0.35" },
+        new Object[]{ Optional.of("251"), "127.0.0.251" }
+      };
+   }
+
+   @Test
+   @Parameters(method = "getInvalidTestIds")
+   public void getLocalIpFromInvalidIdShouldBeEmpty(Optional<String> id) throws Exception {
+      assertThat(Dialogs.getLocalIpFromId(id),equalTo(Optional.empty()));
+   }
+
+   @Test
+   @Parameters(method = "getValidTestIds")
+   public void getLocalIpFromValidIdShouldBeValidIp(Optional<String> id, String ip) throws Exception {
+      assertThat(Dialogs.getLocalIpFromId(id).get(),equalTo(ip));
+   }
 }
